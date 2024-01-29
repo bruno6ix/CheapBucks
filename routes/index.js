5ea588ae82f2d3e1fb36e60aece6f8ed012504e5
bruno7even <brunob2013@hotmail.com.ar>
2024-01-29 17:00:32 -0300

    first commit

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/products', function(req, res, next) {
+  res.render('products')
+})
+
+module.exports = router;
